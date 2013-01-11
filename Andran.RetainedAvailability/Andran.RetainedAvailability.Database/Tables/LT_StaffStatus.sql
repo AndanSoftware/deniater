dfc2ef267069e9d9cb53632d03491df346b1e7d9
Andan And <And@And.com>
2013-01-11 22:13:44 +0000

    db

---
+++ Andran.RetainedAvailability/Andran.RetainedAvailability.Database/Tables/LT_StaffStatus.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[LT_StaffStatus]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Status] VARCHAR(10) NOT NULL
+)
--- Andran.RetainedAvailability/Andran.RetainedAvailability.Database/Tables/LT_UnavailabilityReason.sql
+++ Andran.RetainedAvailability/Andran.RetainedAvailability.Database/Tables/LT_UnavailabilityReason.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[LT_UnavailabilityReason]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Symbol] VARCHAR NOT NULL, 
     [ColourHex] VARCHAR(8) NULL, 
     [Icon] VARCHAR(20) NULL, 
