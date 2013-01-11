4bfa0783728124cb96f638ccdc36c0f5ac3ea9bd
Jordan Beagle <andran.jbeagle@gmail.com>
2013-01-11 21:37:57 +0000

    Check in for sync
    Ignore these for now, had to check in before I could sync. Just having a
    play with whats best to do. Thinking Abstract class which will implement
    IsDirty method perhaps?

---
+++ Andran.RetainedAvailability/Andran.RetainedAvailability.Data/Entities/Appliance.cs
@@ -0,0 +1,14 @@
+﻿using Andran.RetainedAvailability.Data.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andran.RetainedAvailability.Data.Entities
+{
+    public class Appliance : IEntity
+    {
+
+    }
+}
