ae97dcf71ca2fedae483d197cceb06f3b095d099
Andan And <And@And.com>
2013-01-11 22:36:35 +0000

    db fk fix

--- Andran.RetainedAvailability/Andran.RetainedAvailability.Database/Tables/Link_Appliance_Skill.sql
+++ Andran.RetainedAvailability/Andran.RetainedAvailability.Database/Tables/Link_Appliance_Skill.sql
@@ -4,5 +4,5 @@
     [SkillId] INT NOT NULL, 
     PRIMARY KEY ([SkillId], [ApplianceId]), 
     CONSTRAINT [FK_Link_Appliance_Skill_ToTableSkill] FOREIGN KEY ([SkillId]) REFERENCES [Skill]([Id]), 
-    CONSTRAINT [FK_Link_Appliance_Skill_ToTableAppliance] FOREIGN KEY ([ApplianceId]) REFERENCES [Appliance]([ApplianceId]) 
+    CONSTRAINT [FK_Link_Appliance_Skill_ToTableAppliance] FOREIGN KEY ([ApplianceId]) REFERENCES [Appliance]([Id]) 
 )
