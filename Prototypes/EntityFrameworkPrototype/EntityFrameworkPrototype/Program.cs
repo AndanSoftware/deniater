af9e820eb0c86df9706150aebb626de16d465cbe
Andan A <andan@soft.com>
2013-01-13 01:32:38 +0000

    prototype

---
+++ Prototypes/EntityFrameworkPrototype/EntityFrameworkPrototype/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RAEntities r = new RAEntities();
+
+            foreach(var rr in r.LT_UnavailabilityReason)
+            {
+                Console.WriteLine(rr.Symbol + " = " + rr.Reason);
+            }
+            Console.ReadKey();
+        }
+    }
+}
