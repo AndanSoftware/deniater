70ff9211fdabde19e2074174500a549f5fabc0db
Andan A <andan@soft.com>
2013-01-14 19:53:42 +0000

    Initial checking of test data populator

---
+++ Application/Andran.RetainedAvailability.TestDataPopulator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andran.RetainedAvailability.TestDataPopulator
+{
+    class Program
+    {
+        public static string[] firstNames = { "Andrew", "Jordan", "Sarah", "Frank", "Bob", "Gemma" };
+
+        public static string lastNames = { "Critchley", "Turnbull", "Turner", "Beagle", "Smith", "Roberts", "Jones", "Chelsea" };
+        
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Application/Andran.RetainedAvailability.TestDataPopulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Andran.RetainedAvailability.TestDataPopulator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Andran.RetainedAvailability.TestDataPopulator")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d79970a-9d2f-488a-85f3-c4f013ff3ad2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
