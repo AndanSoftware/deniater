74743702e99722bf0d22580d4f0acbb7efb571dc
Andan A <andan@soft.com>
2013-01-14 20:58:37 +0000

    Implemented returning uav for a station for a given day

--- Application/Andran.RetainedAvailability.DAL/Repositories/UnavailabilityRepository.cs
+++ Application/Andran.RetainedAvailability.DAL/Repositories/UnavailabilityRepository.cs
@@ -27,5 +27,30 @@ namespace Andran.RetainedAvailability.DAL.Repositories
 
 
         }
+
+        public IList<Unavailability> GetUnavailabilityForADay(Guid stationId, DateTime dateToRetrieve)
+        {
+            var r = _ctx.Unavailability
+                // Include the reason navigation property
+                .Include("UnavailabilityReason")
+
+                // Only get the crew members for the current station
+                .Join(_ctx.CrewMembers,
+                          uav => uav.CrewMemberID
+                          , cm => cm.CrewMemberID
+                          , (uav, cm) => new { CM = cm, UAV = uav })
+
+                // Then we need to only look at the crew members for the current station
+                .Join(_ctx.Stations,
+                          station => station.CM.StationID
+                          , station => station.StationID
+                          , (uav, station) => uav)
+
+                .Select(u => u.UAV)
+
+                // Only retrieve unavailability for the date passed in
+                .Where(uav => uav.Start.Date == dateToRetrieve.Date);
+
+        }
     }
 }
